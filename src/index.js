68460a8a0e5f771f9531a1281cbace7f697b464b
Serhii Rybak <rybaksw@gmail.com>
2022-05-26 15:53:53 +0300

    add Pixabay api service
    *impl class for fetch from Pixabay
    *add template for image card
    *add a bit style for cards

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import './sass/main.scss';
-import imgCard from './templates/img-card.hbs';
-import fetchImgs from './js/api-pixabay';
+import imgCardTmpl from './templates/img-card.hbs';
+import PixabayApiService from './js/api-pixabay';
 import { Notify } from 'notiflix';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const myPixabay = new PixabayApiService();
+
+refs.searchForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  myPixabay.query = e.currentTarget.elements.searchQuery.value;
+  myPixabay.resetPage();
+  myPixabay.fetchImgs().then(renderImgCards);
+}
+
+function renderImgCards(imgs) {
+  refs.gallery.insertAdjacentHTML('beforeend', imgCardTmpl(imgs));
+}
--- src/js/api-pixabay.js
+++ src/js/api-pixabay.js
@@ -1,3 +1,77 @@
-import axios from 'axios';
+const axios = require('axios');
 
-export default function fetchImgs(tag) {}
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '27629620-bf8dbf1d2d77ad53435eb2e20';
+
+export default class PixabayApiService {
+  #totalHits;
+  #searchQuery;
+
+  constructor() {
+    this.#searchQuery = '';
+    this.imageType = 'photo';
+    this.orientation = 'horizontal';
+    this.safeSearch = 'true';
+    this.perPage = 40;
+    this.page = 1;
+  }
+  /**
+   * Fetch images from Pixabay.
+   * @returns array of objects of images.
+   */
+  fetchImgs() {
+    const reqParams = {
+      key: API_KEY,
+      q: this.#searchQuery,
+      image_type: this.imageType,
+      orientation: this.orientation,
+      safesearch: this.safeSearch,
+      per_page: this.perPage,
+      page: this.page,
+    };
+
+    return axios.get(BASE_URL, { params: reqParams }).then(({ data }) => {
+      console.log(this);
+      console.log(data);
+
+      this.#numOfResults = data.totalHits;
+      this.page += 1;
+      // if (this.#numOfResults > 0) {
+      return data.hits;
+      //   .map(
+      //   ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      //     return { webformatURL, largeImageURL, tags, likes, views, comments, downloads };
+      //   }
+      // );
+      // }
+    });
+  }
+  /**
+   * Set request parameter 'page' to 1.
+   */
+  resetPage() {
+    this.page = 1;
+  }
+  /**
+   * Get search query value.
+   */
+  get query() {
+    return this.#searchQuery;
+  }
+  /**
+   * Set search query value.
+   */
+  set query(newQuery) {
+    this.#searchQuery = newQuery;
+  }
+  /**
+   * Return the number of images found.
+   */
+  get numOfResults() {
+    return this.#totalHits;
+  }
+
+  set #numOfResults(num) {
+    this.#totalHits = num;
+  }
+}
